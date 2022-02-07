78b763bf7f109ef65a33e11131e9dda4232fd956
unknown <dusi.abhinav2002@gmail.com>
2022-02-07 17:52:29 -0500

    Algorithmic Player

--- AlgorithmicPlayer.hpp
+++ AlgorithmicPlayer.hpp
@@ -8,12 +8,19 @@ using namespace std;
 
 class AlgorithmicPlayer: public Player {
     public:
-    AlgorithmicPlayer(int id): Player(id) {};
+    AlgorithmicPlayer(int id, int width, int height): 
+        Player(id), _width(width), _height(height), _first_to_move(id == 1) {};
     int get_move(Game& game); 
     string get_name() { return "Algorithmic"; }
+
+    private:
+    int _width;
+    int _height;
+    bool _first_to_move;
 }; 
 
 int AlgorithmicPlayer::get_move(Game &game) {
+    bool odd_chains = _first_to_move && (_width%2==0) && (_height%2==0); 
     return 0;
 }
 

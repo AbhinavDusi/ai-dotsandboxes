f3f76dfacf91cfdc1f3a20dc23b633218da6a485
unknown <dusi.abhinav2002@gmail.com>
2022-07-06 02:44:47 -0700

    Updated algorithmic

--- include/Player/Algorithmic/AlgorithmicPlayer.hpp
+++ include/Player/Algorithmic/AlgorithmicPlayer.hpp
@@ -11,16 +11,9 @@ class AlgorithmicPlayer: public Player {
     AlgorithmicPlayer(int id): Player(id) {};
     int get_move(Game& game); 
     string get_name() { return "Algorithmic"; }
-
-    private:
-    int _width;
-    int _height;
-    bool _first_to_move;
 }; 
 
 int AlgorithmicPlayer::get_move(Game &game) {
-    GameImage game_image = game._game_image;
-    
     return 0;
 }
 

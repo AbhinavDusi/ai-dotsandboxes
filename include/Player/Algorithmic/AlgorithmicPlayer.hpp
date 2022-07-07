6b256a3c16a84bc5126c3990d2e2d7ea8f53d13e
Abhinav Dusi <dusi.abhinav2002@gmail.com>
2022-07-07 00:03:16 -0700

    Get chains

--- include/Player/Algorithmic/AlgorithmicPlayer.hpp
+++ include/Player/Algorithmic/AlgorithmicPlayer.hpp
@@ -11,9 +11,31 @@ class AlgorithmicPlayer: public Player {
     AlgorithmicPlayer(int id): Player(id) {};
     int get_move(Game &game); 
     string get_name() { return "Algorithmic"; }
+
+    private:
+    vector<vector<pair<int, int>>> get_open_chains(Game &game) const;
+    vector<vector<pair<int, int>>> get_closed_chains(Game &game) const;
 }; 
 
+vector<vector<pair<int, int>>> AlgorithmicPlayer::get_open_chains(Game &game) const {
+
+}
+
+vector<vector<pair<int, int>>> AlgorithmicPlayer::get_closed_chains(Game &game) const {
+
+}
+
 int AlgorithmicPlayer::get_move(Game &game) {
+    vector<Move> moves = game._moves;
+
+    vector<vector<pair<int, int>>> open_chains = get_open_chains(game);
+    vector<vector<pair<int, int>>> closed_chains = get_closed_chains(game);
+
+    int total_chained_boxes = 0; 
+    for (vector<pair<int, int>> v : open_chains) total_chained_boxes += v.size();
+    for (vector<pair<int, int>> v : closed_chains) total_chained_boxes += v.size();
+    bool only_chains = total_chained_boxes == game._width * game._height;
+    
     return 0;
 }
 
